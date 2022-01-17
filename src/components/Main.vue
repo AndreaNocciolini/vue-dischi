292996cedb0a751135efc9b23fd79fe281f9b7ba
AndreaNocciolini <andreanocciolini93@gmail.com>
2022-01-17 18:38:32 +0100

    add loading screen

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="main-container">
-    <div class="albums-container row row-cols-5 g-3">
+    <div v-if="albums" class="albums-container row row-cols-5 g-3">
       <div v-for="(album, index) in albums" :key="index" class="col p-3">
         <div class="bg-album p-3 album-height">
           <img class="mb-2" :src="album.poster" :alt="album.title">
-          <h2 class="text-light mb-3 title-uppercase fs-4">{{ album.title }}</h2>
+          <h2 class="text-light mb-3 text-uppercase fs-4">{{ album.title }}</h2>
           <h3 class="text-muted mb-0 fs-4">{{ album.author }}</h3>
           <h4 class="text-muted fs-5">{{ album.year }}</h4>
         </div>
+      </div>
+    </div>
+    <div v-else>
+      <div class="loading-screen">
+        <h1 class="font-weight-bold">Loading...</h1>
       </div>
     </div>
   </div>
@@ -47,9 +52,6 @@ export default {
       margin: 0 auto;
     }
   }
-  .title-uppercase {
-    text-transform: uppercase;
-  }
   img {
     width: 100%;
   }
@@ -58,5 +60,11 @@ export default {
   }
   .album-height {
     height: 100%;
+  }
+  .loading-screen {
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
   }
 </style>
