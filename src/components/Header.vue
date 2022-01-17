9e12414c81b7a61a2a7ad11484ec7335af1eb5dc
AndreaNocciolini <andreanocciolini93@gmail.com>
2022-01-17 15:35:27 +0100

    start

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="header-container">
+      <div class="logo-container">
+          <img src="../assets/images/Spotify_logo_without_text.png" alt="alt">
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+}
+</script>
+
+<style lang="scss" scoped>
+    .header-container {
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+        width: 100%;
+        background-color: #2E3A46;
+        .logo-container {
+            width: 3%;
+            img {
+                width: 100%;
+                padding: 1em
+            }
+        }
+    }
+</style>
